663b0ceac45262b455ee96fb5bd976158410f6c6
Daniel Hernandez <daniel@subseven.io>
2022-04-09 15:58:03 -0500

    Update esbuild.config.js

--- esbuild.config.js
+++ esbuild.config.js
@@ -1,19 +1,11 @@
 const build = require("./config/esbuild.defaults.js")
 const path = require("path")
 const sassPlugin = require ("esbuild-sass-plugin").default
-const postcss = require('postcss')
-const autoprefixer = require('autoprefixer')
-const postcssPresetEnv = require('postcss-preset-env')
 // Update this if you need to configure a destination folder other than `output`
 const outputFolder = "output"
 
 const esbuildOptions = {
-    plugins: [sassPlugin({
-      async transform(source, resolveDir) {
-        const {css} = await postcss([autoprefixer, postcssPresetEnv({stage: 0})]).process(source)
-        return css
-      }
-    })]
+    plugins: [sassPlugin()]
   }
 
 build(outputFolder, esbuildOptions)
