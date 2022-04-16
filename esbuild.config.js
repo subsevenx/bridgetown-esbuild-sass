59e2cae1aa1e21f0681a338d50087ab1b3ffb8ab
Daniel Hernandez <daniel@subseven.io>
2022-04-16 02:19:30 -0500

    Update esbuild.config.js

--- esbuild.config.js
+++ esbuild.config.js
@@ -1,11 +1,27 @@
 const build = require("./config/esbuild.defaults.js")
 const path = require("path")
 const sassPlugin = require ("esbuild-sass-plugin").default
+// Base plugins that ship with Bridgetown
+const postCSS = require('postcss')([
+  require('autoprefixer'),
+  require('postcss-flexbugs-fixes'),
+  require('postcss-preset-env')({
+    autoprefixer: {
+      flexbox: 'no-2009'
+    },
+    stage: 2
+    }),
+])
 // Update this if you need to configure a destination folder other than `output`
 const outputFolder = "output"
 
 const esbuildOptions = {
-    plugins: [sassPlugin()]
+  plugins: [sassPlugin({
+    async transform(source) {
+      const {css} = await postCSS.process(source, {from: undefined})
+      return css
+    }
+  })]
   }
 
 build(outputFolder, esbuildOptions)
